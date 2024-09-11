49ff2a09ee9ea8b7b2155c48c257898888a20218
mtss78 <mathis.mdjie@gmail.com>
2024-09-11 16:54:58 +0200

    envoie

---
+++ faq.php
@@ -0,0 +1,33 @@
+<?php
+    include 'partials/head.php';
+?>
+
+    <section class="about-us">
+        <div class="container1">
+            <h2 class="title78">Qui sommes-nous ?</h2>
+            <p>
+                Bienvenue chez <strong>le trésor du lapin</strong>, votre destination en ligne dédiée aux accessoires, jouets et 
+                à la nourriture de qualité pour vos adorables lapins. Fondée par des passionnés de lapins, notre boutique a 
+                pour mission d'offrir à vos petits compagnons tout ce dont ils ont besoin pour être heureux, en bonne santé 
+                et stimulés.
+            </p>
+            <p>
+                Nous croyons que chaque lapin mérite le meilleur, c’est pourquoi nous sélectionnons soigneusement nos produits, 
+                en nous assurant qu'ils respectent les besoins spécifiques de vos animaux. Que ce soit pour leur bien-être, 
+                leur alimentation ou leur divertissement, nous avons tout ce qu'il faut pour faire plaisir à vos lapins.
+            </p>
+            <p>
+                Chez <strong>le trésor du lapin</strong>, nous nous engageons également à respecter l'environnement en proposant des 
+                produits durables et éthiques. Nous travaillons avec des fournisseurs qui partagent nos valeurs et qui mettent 
+                en avant le bien-être animal.
+            </p>
+            <p>
+                Rejoignez-nous dans notre aventure et offrez à votre lapin un monde de confort, de joie et de santé !
+            </p>
+        </div>
+    </section>
+
+
+<?php
+    include 'partials/footer.php';
+?>
