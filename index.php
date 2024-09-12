f52b15658401182e4aa3d53b695332a3b8df4bee
mtss78 <mathis.mdjie@gmail.com>
2024-09-12 09:35:34 +0200

    nav

--- index.php
+++ index.php
@@ -9,17 +9,16 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
-
 <body>
     <nav class="navbar navbar-expand-lg bg-white">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
-                <img src="/public/img/Rabbit Line Logo (1).png" alt="Logo lapin" width="40" height="40" class="d-inline-block align-text-top">
+                <img src="/public/img/Rabbit Line Logo (1).png" alt="Logo le Poles" width="30" height="30" class="d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse justify-content-end fw-semibold " id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" href="/">Nourriture</a>
@@ -31,6 +30,9 @@
                         <a class="nav-link active" href="/">Accessoires</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link active" href="/">Contactez-Nous</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link active" href="/">Qui Sommes-Nous</a>
                     </li>
                 </ul>
@@ -38,5 +40,4 @@
         </div>
     </nav>
 </body>
-
 </html>
